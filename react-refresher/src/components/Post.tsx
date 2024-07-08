1d9aab98fa149e8ca0d42e00e30207db67dee106
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-08 14:45:19 +0900

    19. Outputting Dynamic Values

--- react-refresher/src/components/Post.tsx
+++ react-refresher/src/components/Post.tsx
@@ -1,7 +1,12 @@
+const names = ['Geryenko', 'Hawsen'];
+
 function Post() {
+  // chosenName is recalculated every time the Post component renders
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Gery</p>
+      <p>{chosenName}</p>
       <p>React Refresher</p>
     </div>
   );
