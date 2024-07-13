cf0dae9168ac736a646ad496b163bd121fedf10e
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-14 01:27:17 +0900

    40. Adding Routes

--- react-refresher/src/main.tsx
+++ react-refresher/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import NewPost from './components/NewPost';
+
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/create-post',
+    element: <NewPost />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
