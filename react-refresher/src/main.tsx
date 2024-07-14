ca321f6dc992cef79c6eef5ce8096c43c0db6fd7
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-14 12:09:21 +0900

    41. Working with Layout Routes

--- react-refresher/src/main.tsx
+++ react-refresher/src/main.tsx
@@ -3,17 +3,24 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout';
 
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-  },
-  {
-    path: '/create-post',
-    element: <NewPost />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/create-post',
+        element: <NewPost />,
+      },
+    ],
   },
 ]);
 
