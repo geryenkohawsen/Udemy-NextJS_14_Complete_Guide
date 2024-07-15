bddcc9e9a4025b0d102d5e212e3d57d05c607b0b
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-16 00:30:49 +0900

    104. Preparing an Image Slideshow

---
+++ components/images/image-slideshow.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+import curryImg from "@/assets/curry.jpg";
+import pizzaImg from "@/assets/pizza.jpg";
+import burgerImg from "@/assets/burger.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import schnitzelImg from "@/assets/schnitzel.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
+import tomatoSaladImg from "@/assets/tomato-salad.jpg";
+
+import styles from "./image-slideshow.module.css";
+
+const images = [
+  { image: burgerImg, alt: "A delicious, juicy burger" },
+  { image: curryImg, alt: "A delicious, spicy curry" },
+  { image: dumplingsImg, alt: "Steamed dumplings" },
+  { image: macncheeseImg, alt: "Mac and cheese" },
+  { image: pizzaImg, alt: "A delicious pizza" },
+  { image: schnitzelImg, alt: "A delicious schnitzel" },
+  { image: tomatoSaladImg, alt: "A delicious tomato salad" },
+];
+
+export default function ImageSlideshow() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex < images.length - 1 ? prevIndex + 1 : 0,
+      );
+    }, 5000);
+
+    //! Cleanup Function
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={styles.slideshow}>
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          src={image.image}
+          className={index === currentImageIndex ? styles.active : ""}
+          alt={image.alt}
+        />
+      ))}
+    </div>
+  );
+}
