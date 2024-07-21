ead91617fae34cef659dcc3e07e50d55e352982e
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-21 17:46:33 +0900

    118. Adding an Image Preview to the Picker

--- components/meals/image-picker.tsx
+++ components/meals/image-picker.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import styles from "./image-picker.module.css";
+import Image from "next/image";
 
 interface Props {
   label: string;
@@ -9,16 +10,48 @@ interface Props {
 }
 
 export default function ImagePicker({ label, name }: Props) {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
   const imageInputRef = useRef<HTMLInputElement>(null);
 
   function handlePickClick() {
     imageInputRef.current?.click();
   }
 
+  function handleImageChange(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files) {
+      setPickedImage(null);
+      return;
+    }
+
+    const file = event.target.files[0];
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      if (!fileReader.result) return;
+
+      setPickedImage(
+        typeof fileReader.result === "string"
+          ? fileReader.result
+          : Buffer.from(fileReader.result).toString(),
+      );
+    };
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={styles.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
         <input
           className={styles.input}
           type="file"
@@ -26,6 +59,7 @@ export default function ImagePicker({ label, name }: Props) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInputRef}
+          onChange={handleImageChange}
         />
         <button
           className={styles.button}
