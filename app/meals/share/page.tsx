61fcfa7bae9a29df74be7676490530e76452b282
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-21 21:50:28 +0900

    120. Introducing & Using Server Actions for Handling Form Submissions

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,7 +1,25 @@
 import ImagePicker from "@/components/meals/image-picker";
 import styles from "./page.module.css";
+import { FormEvent } from "react";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData: FormData) {
+    "use server";
+
+    console.log("formData --> ", formData);
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log("meal --> ", meal);
+  }
+
   return (
     <>
       <header className={styles.header}>
@@ -11,7 +29,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={shareMeal}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +57,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label={""} name={""} />
+          <ImagePicker label={"Your image"} name={"image"} />
           <p className={styles.actions}>
             <button type="submit">Share Meal</button>
           </p>
