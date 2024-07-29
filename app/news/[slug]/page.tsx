98a64e5e52447d717cff7d005f56992e6a88b71d
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-30 01:14:24 +0900

    140. Handling "Not Found" Errors & Showing "Not Found" Pages

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { DUMMY_NEWS } from '@/dummy-news'
+import { notFound } from 'next/navigation'
 
 interface Props {
   params: { slug: string }
@@ -9,21 +10,17 @@ export default function NewsDetailPage({ params }: Props) {
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug)
 
   if (!newsItem) {
-    return (
-      <article className="news-article">
-        <h1>News not found</h1>
-      </article>
-    )
-  } else {
-    return (
-      <article className="news-article">
-        <header>
-          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-          <h1>{newsItem.title}</h1>
-          <time dateTime={newsItem.date}>{newsItem.date}</time>
-        </header>
-        <p>{newsItem.content}</p>
-      </article>
-    )
+    notFound()
   }
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  )
 }
