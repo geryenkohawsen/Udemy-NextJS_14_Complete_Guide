c979300364fee7658b10453d13b9cdc08f147ad3
Geryenko Hawsen <gery.zarons@gmail.com>
2024-07-29 15:32:20 +0900

    137. Exercise Solution - Part 1

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,9 +1,14 @@
-export default function NewsDetailPage({ params }: { params: { id: string } }) {
-  console.log('params --> ', params)
+interface Props {
+  params: { id: string }
+}
+
+export default function NewsDetailPage({ params }: Props) {
+  const newsId = params.id
 
   return (
     <div>
       <h1>News Detail Page</h1>
+      <p>News ID: {newsId}</p>
     </div>
   )
 }
