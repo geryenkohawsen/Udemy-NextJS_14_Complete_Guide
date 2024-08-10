d0cf3bff12ca9101ba8a01f3ef55fc1a4be9b71e
Geryenko Hawsen <gery.zarons@gmail.com>
2024-08-10 22:38:34 +0900

    152. Navigating Programmatically

--- app/news/[slug]/@modal/(.)image/page.tsx
+++ app/news/[slug]/@modal/(.)image/page.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import { DUMMY_NEWS } from '@/dummy-news'
-import { notFound } from 'next/navigation'
+import { notFound, useRouter } from 'next/navigation'
 
 interface Props {
   params: { slug: string }
 }
 
 export default function InterceptedImagePage({ params }: Props) {
+  const router = useRouter()
+
   const newsItemSlug = params.slug
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemSlug)
 
@@ -15,7 +19,7 @@ export default function InterceptedImagePage({ params }: Props) {
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
