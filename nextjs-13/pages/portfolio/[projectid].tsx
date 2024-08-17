41f3c26010142caf473ec12a1d33f6fa110b8934
Geryenko Hawsen <gery.zarons@gmail.com>
2024-08-17 17:00:55 +0900

    234. Adding Dynamic Paths & Routes

---
+++ nextjs-13/pages/portfolio/[projectid].tsx
@@ -0,0 +1,11 @@
+interface Props {
+  params: { projectid: string }
+}
+
+export default function PortfolioProjectPage({ params }: Props) {
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  )
+}
