e64eedf1262ce9a6de491e95a693fa7e14d75d6a
Geryenko Hawsen <gery.zarons@gmail.com>
2024-08-18 14:56:26 +0900

    241. Navigating Programmatically

--- nextjs-13/pages/clients/[id]/index.tsx
+++ nextjs-13/pages/clients/[id]/index.tsx
@@ -5,9 +5,18 @@ export default function ClientsProjectsPage() {
 
   console.log('router.query', router.query)
 
+  function loadProjectHandler() {
+    // router.push('/clients/max/project-a')
+    router.push({
+      pathname: '/clients/[id]/[cilentprojectid]',
+      query: { id: 'max', cilentprojectid: 'projecta' },
+    })
+  }
+
   return (
     <div>
       <h1>The Clients Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
