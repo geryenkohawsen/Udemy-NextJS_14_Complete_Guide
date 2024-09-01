0e505e400d340e886f25726a0704e901b1693d79
Geryenko Hawsen <gery.zarons@gmail.com>
2024-09-01 19:19:29 +0900

    257. Working on the "All Events" Page

--- nextjs-13/pages/events/index.tsx
+++ nextjs-13/pages/events/index.tsx
@@ -1,7 +1,12 @@
+import EventList from '@/components/events/event-list'
+import { getAllEvents } from '@/dummy-data'
+
 export default function AllEventsPage() {
+  const events = getAllEvents()
+
   return (
     <div>
-      <h1>The All Events Page</h1>
+      <EventList items={events} />
     </div>
   )
 }
