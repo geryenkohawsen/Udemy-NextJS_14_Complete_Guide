f8cc1c6e8cdacb2cbde840b0cf0dd86ba5bad98a
Geryenko Hawsen <gery.zarons@gmail.com>
2024-09-01 19:16:49 +0900

    256. Adding a General Layout Wrapper Component

--- nextjs-13/pages/events/[eventid].tsx
+++ nextjs-13/pages/events/[eventid].tsx
@@ -2,7 +2,6 @@ import EventContent from '@/components/event-detail/event-content'
 import EventLogistics from '@/components/event-detail/event-logistics'
 import EventSummary from '@/components/event-detail/event-summary'
 import { getEventById } from '@/dummy-data'
-import image from 'next/image'
 import { useRouter } from 'next/router'
 
 export default function EventDetailPage() {
@@ -21,7 +20,7 @@ export default function EventDetailPage() {
   return (
     <>
       <EventSummary title={event.title} />
-      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={image.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
