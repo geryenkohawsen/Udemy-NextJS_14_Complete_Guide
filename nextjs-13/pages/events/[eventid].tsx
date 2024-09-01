28100e44f75ecdf7da93c394b2f430272909b8be
Geryenko Hawsen <gery.zarons@gmail.com>
2024-09-01 18:29:57 +0900

    255. Adding the "Event Detail" Page (Dynamic Route)

--- nextjs-13/pages/events/[eventid].tsx
+++ nextjs-13/pages/events/[eventid].tsx
@@ -1,11 +1,30 @@
-interface Props {
-  params: { enventid: string }
-}
+import EventContent from '@/components/event-detail/event-content'
+import EventLogistics from '@/components/event-detail/event-logistics'
+import EventSummary from '@/components/event-detail/event-summary'
+import { getEventById } from '@/dummy-data'
+import image from 'next/image'
+import { useRouter } from 'next/router'
 
-export default function EventDetailPage({ params }: Props) {
+export default function EventDetailPage() {
+  const router = useRouter()
+
+  const eventId = router.query.eventid
+  if (!eventId) {
+    return <p>Invalid event ID</p>
+  }
+
+  const event = getEventById(eventId.toString())
+  if (!event) {
+    return <p>Event not found</p>
+  }
+
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={image.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   )
 }
