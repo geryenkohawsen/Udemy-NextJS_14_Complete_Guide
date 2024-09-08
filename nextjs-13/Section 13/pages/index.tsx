15b73b5ac8e507369da4021ec3921f8e4c2156b0
Geryenko Hawsen <gery.zarons@gmail.com>
2024-09-09 03:00:36 +0900

    268. NextJS Pre-renders By Default!

---
+++ nextjs-13/Section 13/pages/index.tsx
@@ -0,0 +1,12 @@
+import EventList from '@/components/events/event-list'
+
+export default function HomePage() {
+
+  return (
+          <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  )
+}
