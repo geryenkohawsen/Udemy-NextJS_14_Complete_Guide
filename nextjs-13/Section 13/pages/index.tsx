01a3f487c1b7da98a5c4649a38b0d34e9068276c
Geryenko Hawsen <gery.zarons@gmail.com>
2024-09-10 20:45:30 +0900

    269. Adding "getStaticProps" To Pages

--- nextjs-13/Section 13/pages/index.tsx
+++ nextjs-13/Section 13/pages/index.tsx
@@ -1,12 +1,26 @@
-import EventList from '@/components/events/event-list'
+interface Props {
+  products: {
+    id: string
+    title: string
+  }[]
+}
 
-export default function HomePage() {
+export default function HomePage(props: Props) {
+  const { products } = props
 
   return (
-          <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: 'p1', title: 'Product 1' }],
+    },
+  }
 }
