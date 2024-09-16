ace9bc97fc78759ec932a00c8e623f36ec350baf
Geryenko Hawsen <gery.zarons@gmail.com>
2024-09-16 14:14:42 +0900

    280. Loading Paths Dynamically

--- nextjs-13/Section 13/pages/[pid].tsx
+++ nextjs-13/Section 13/pages/[pid].tsx
@@ -3,6 +3,13 @@ import fs from 'fs/promises'
 import type { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 import path from 'path'
 
+const getData = async () => {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData.toString()) as DummyData
+  return data
+}
+
 export const getStaticProps: GetStaticProps<LoadedProduct> = async (context) => {
   const { params } = context
 
@@ -14,9 +21,7 @@ export const getStaticProps: GetStaticProps<LoadedProduct> = async (context) => 
 
   const productId = params.pid
 
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
-  const jsonData = await fs.readFile(filePath)
-  const data = JSON.parse(jsonData.toString()) as DummyData
+  const data = await getData()
 
   const product = data.products.find((product) => product.id === productId)
 
@@ -34,15 +39,16 @@ export const getStaticProps: GetStaticProps<LoadedProduct> = async (context) => 
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await getData()
+  const ids = data.products.map((product) => product.id)
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }))
+
   return {
-    paths: [
-      {
-        params: { pid: 'p1' },
-      },
-    ],
+    paths: pathsWithParams,
     // true or false or "blocking"
     // if fallback is true, it tells Next.js that some other params might be available and is a valid path.
     // However, it will only be generated during the request
+    // Blocking will make it wait and only render the DOM after the request completes
     fallback: 'blocking',
   }
 }
