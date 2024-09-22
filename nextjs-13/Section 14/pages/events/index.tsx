4c9fccd0078320d5b5a59ca169e13673058fbad3
Geryenko Hawsen <gery.zarons@gmail.com>
2024-09-22 16:29:35 +0900

    299. Working on the "All Events" Page

--- nextjs-13/Section 14/pages/events/index.tsx
+++ nextjs-13/Section 14/pages/events/index.tsx
@@ -1,11 +1,28 @@
 import EventList from '@/components/events/event-list'
 import EventsSearch from '@/components/events/events-search'
-import { getAllEvents } from '@/dummy-data'
+import type { Event } from '@/helpers/api-util'
+import { getAllEvents } from '@/helpers/api-util'
+import type { GetStaticProps, GetStaticPropsResult, InferGetStaticPropsType } from 'next'
 import { useRouter } from 'next/router'
 
-export default function AllEventsPage() {
+type StaticProps = {
+  events: Event[]
+}
+
+export const getStaticProps = (async (context): Promise<GetStaticPropsResult<StaticProps>> => {
+  const events = await getAllEvents()
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60, // Cache the data for every 1 minute (60 seconds)
+  }
+}) satisfies GetStaticProps
+
+export default function AllEventsPage(props: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter()
-  const events = getAllEvents()
+  const events = props.events
 
   function findEventsHandler(year: string, month: string) {
     const fullPath = `/events/${year}/${month}`
